e9f8ae2f763a92204c9b7e1ef80bc12f745bd4d9
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-06-28 21:11:47 +0530

    FindSlot  Development

--- components/restaurant/FindSlot.tsx
+++ components/restaurant/FindSlot.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 
-const FindSlot = ({slots}) => {
+const FindSlot = ({ date,
+  selectedNumber,
+  slots,
+  selectedSlot,
+  setSelectedSlot }) => {
+
+  const [slotsVisible, setSlotVisisble] = useState(false);
+
+  const handlePress = () => {
+    setSlotVisisble(!slotsVisible)
+  }
+
   return (
-    <View>
-      <Text>FindSlot</Text>
+    <View className="flex-1 w-full">
+      <View className={`flex ${selectedSlot != null && "flex-row ml-5"} w-full px-2`}>
+        <View
+          className = {`${selectedSlot != null && 'flex-1'}`}
+        >
+          <TouchableOpacity
+            className="w-full"
+            onPress={handlePress}
+          >
+            <Text className="text-lg text-center font-semibold bg-[#f49b33] p-2 my-3 rounded-lg">
+              Find Slots
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          className = {`${selectedSlot != null && 'flex-1'}`}
+        >
+          <TouchableOpacity
+            className="w-full"
+            onPress={handlePress}
+          >
+            <Text className="text-lg text-white text-center font-semibold bg-[#f49b33] p-2 my-3 rounded-lg">
+              Book Now
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
